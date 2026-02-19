72f2f8f845806ef8f334261349cc514232db56b8
MaheshwariVallapu <118046805+MaheshwariVallapu@users.noreply.github.com>
2026-02-18 23:36:24 -0500

    Update profile.js

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,7 +3,7 @@ const PROFILE = {
   linkedin: "https://www.linkedin.com/in/maheshwari-vallapu-86b0641b1/",
   github: "https://github.com/MaheshwariVallapu",
   medium: "https://medium.com/@maheshwarimahi9949",
-  resume: "https://1drv.ms/w/c/22bc417149e2b389/IQCiblFm4x-_SZAR9NqyeZ2sAb3915dRCLU02FWyG1X4z-4?e=BQdOI8"
+  resume: "https://1drv.ms/w/c/22bc417149e2b389/IQCSWbaA8ZcbRqDrnvan47QPAduqcqxcv7YLDuo-rcMh4zI?e=dab0LK"
 };
 
 function setHref(id, url) {
